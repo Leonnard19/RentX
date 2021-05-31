b9ae88cb4918680785cdf84c164ebf590f6ce142
Leonardo <srleon19@hotmail.com>
2021-05-31 15:25:49 -0300

    Car Images

--- src/modules/accounts/useCases/authenticateUser/authenticateUserController.ts
+++ src/modules/accounts/useCases/authenticateUser/authenticateUserController.ts
@@ -1,6 +1,6 @@
 import { container } from 'tsyringe';
 import { Request, Response } from 'express';
-import { AuthenticateUserUseCase } from './authenticateUserUseCase';
+import { AuthenticateUserUseCase } from './AuthenticateUserUseCase';
 
 class AuthenticateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,12 +1,16 @@
 import { container } from 'tsyringe';
 
-import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
-import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
-import { SpecificationRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
+import { UsersRepository } from '../../modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { CategoriesRepository } from '../../modules/cars/infra/typeorm/repositories/CategoriesRepository';
+import { SpecificationRepository } from '../../modules/cars/infra/typeorm/repositories/SpecificationsRepository';
+import { CarsRepository } from '../../modules/cars/infra/typeorm/repositories/CarsRepository';
+import { CarsImagesRepository } from '../../modules/cars/infra/typeorm/repositories/CarsImagesRepository';
 
 import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
+import { ICarsRepository } from '../../modules/cars/repositories/ICarsRepository';
+import { ICarsImagesRepository } from '../../modules/cars/repositories/ICarsImagesRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
@@ -18,4 +22,11 @@ container.registerSingleton<ISpecificationsRepository>(
   SpecificationRepository
 );
 
+container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
+
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
+
+container.registerSingleton<ICarsImagesRepository>(
+  'CarsImagesRepository',
+  CarsImagesRepository
+);
