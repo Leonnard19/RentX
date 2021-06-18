97dbd5055d8b3ab4d71d2f842795d71aebf59969
Leonardo <srleon19@hotmail.com>
2021-06-18 00:26:43 -0300

    refactoring

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -5,7 +5,20 @@ import { EtherealMailProvider } from './MailProvider/implementations/EtherealMai
 
 import { IDateProvider } from './dateProvider/IDateProvider';
 import { IMailProvider } from './MailProvider/IMailProvider';
+import { IStorageProvider } from './StorageProvider/IStorageProvider';
+import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider';
+import { S3StorageProvider } from './StorageProvider/implementations/S3StorageProvider';
 
 container.registerSingleton<IDateProvider>('DayjsDateProvider', DayjsDateProvider);
 
 container.registerInstance<IMailProvider>('EtherealMailProvider', new EtherealMailProvider());
+
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  diskStorage[process.env.disk]
+);
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -12,7 +12,7 @@ import multer from 'multer';
 
 const carRoutes = Router();
 
-const upload = multer(uploadConfig.upload('./tmp/cars'));
+const upload = multer(uploadConfig);
 
 const createCarController = new CreateCarController();
 const listCarsController = new ListCarsController();
